e6a542a6f8bf1ca131dd87b119b527743b90f886
sairam-gwu <sairam.venkatachalam@gwu.edu>
2023-10-12 18:52:56 -0400

    Update EDA Sales and Categories.R

--- EDA Sales and Categories.R
+++ EDA Sales and Categories.R
@@ -84,10 +84,97 @@ t_test_rating <- t.test(rating_above$Global_Sales,rating_below$Global_Sales,var.
 
 t_test_rating
 
+  
+
+  
+# Confidence Interval shooter games
+  
+  
+  
+library(dplyr)
+library(stats)
+  
+  
+  
+subset_data <- games %>% filter(Genre == 'Shooter' & User_Score > 8.0)
+  
+sample_proportion <- nrow(subset_data) / nrow(games)
+  
+confidence_level <- 0.95
+  
+standard_error <- sqrt((sample_proportion * (1 - sample_proportion)) / nrow(games))
+  
+df <- nrow(games) - 1  # Degrees of freedom
+t_critical <- qt(1 - (1 - confidence_level) / 2, df)  # t-critical value
+margin_of_error <- t_critical * standard_error
+
+# Calculate the confidence interval
+lower_bound <- sample_proportion - margin_of_error
+upper_bound <- sample_proportion + margin_of_error
+
+# Print the results
+cat("Sample Proportion:", sample_proportion, "\n")
+cat("95% Confidence Interval :", lower_bound, "to", upper_bound, "\n")
+
+# Rating vs Sales
 
 ggplot(games, aes(x = User_Score, y = Global_Sales, color = User_Score >= 7.5)) +
   geom_point() +
   scale_color_manual(values = c("red", "green")) +
   labs(x = "User Score", y = "Global Sales", title = "Scatter Plot of User Score vs. Global Sales") +
-  guides(color = guide_legend(title = "User_Score")) +
+  guides(color = guide_legend(title = "User Score greater than 7.5")) +
   theme_minimal()
+
+
+#
+
+
+
+
+# Iterating to find threshold
+
+
+best_rating_split <- 2.0
+best_diff_means <- 0
+best_p_value <- 1  
+
+# Define the minimum threshold difference in means and maximum p-value
+min_threshold_difference <- 0.3  # Adjust as needed
+max_p_value <- 0.05  # Adjust as needed
+
+# Iterate over user rating thresholds from 4.0 to 8.0 in 0.1 increments
+for (rating_threshold in seq(4.0, 8.0, by = 0.1)) {
+  # Subset data for games with user scores above and below the threshold
+  rating_above <- subset(games, games$User_Score >= rating_threshold)
+  rating_below <- subset(games, games$User_Score < rating_threshold)
+  
+  # Calculate the sample means
+  mean_sales_above <- mean(rating_above$Global_Sales)
+  mean_sales_below <- mean(rating_below$Global_Sales)
+  
+  # Calculate the difference in means
+  diff_means <- mean_sales_above - mean_sales_below
+  
+  # Perform a t-test for the two groups with unequal variances
+  t_test_result <- t.test(rating_above$Global_Sales, rating_below$Global_Sales, var.equal = FALSE)
+  
+  # Check if the p-value is below the maximum
+  if (t_test_result$p.value < max_p_value) {
+    # Check if the difference in means is greater than the best result
+    if (diff_means > best_diff_means) {
+      best_rating_split <- rating_threshold
+      best_diff_means <- diff_means
+      best_p_value <- t_test_result$p.value
+    }
+  }
+}
+
+# Print the best result
+cat("Best rating split with the highest difference in means and p-value below", max_p_value, "is:", best_rating_split, "\n")
+cat("Mean Difference in Sales:", best_diff_means, "\n")
+cat("P-Value for the T-Test:", best_p_value, "\n")
+
+
+
+
+
